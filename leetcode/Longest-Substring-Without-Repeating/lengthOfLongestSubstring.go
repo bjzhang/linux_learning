a3739fe243ac7776c3a1c9680ff3f800dd244190
Bamvor Zhang <bamv2005@gmail.com>
2019-01-07 18:39:57 +0800

    leetcode: lengthOfLongestSubstring: the first correct answer
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- leetcode/Longest-Substring-Without-Repeating/lengthOfLongestSubstring.go
+++ leetcode/Longest-Substring-Without-Repeating/lengthOfLongestSubstring.go
@@ -2,32 +2,37 @@ package main
 
 import "fmt"
 import "os"
+import "strings"
 
 func lengthOfLongestSubstring(s string) int {
 	if len(s) == 1 {
 		return 1
 	}
+	lastMax := 0
+	lastMaxString := ""
 	for i := 0; i < len(s); i++ {
-		fmt.Printf("outter loop: %d: %c\n", i, s[i])
-		lastChar := s[i]
 		curMax := 1
+		curMaxString := ""
 		for j := i + 1; j < len(s); j++ {
-			fmt.Printf("inner loop: %d: %c(last char: %c)\n", j, s[j], lastChar)
-			fmt.Printf("current string: %s\n", s[i:j+1])
-			if s[j] == lastChar {
+			if strings.Contains(s[i:j], s[j:j+1]) {
 				curMax = j - i
-				fmt.Printf("CURRENT max length string: %d, %s\n", curMax, s[i:j])
+				curMaxString = s[i:j]
 				break
 			} else if j == len(s)-1 {
 				curMax = j - i + 1
-				fmt.Printf("CURRENT max length string: %d, %s\n", curMax, s[i:j+1])
+				curMaxString = s[i : j+1]
 			}
-			lastChar = s[j]
+		}
+		if curMax > lastMax {
+			lastMax = curMax
+			lastMaxString = curMaxString
 		}
 	}
-	return 1
+	fmt.Printf("max string %s, %d\n", lastMaxString, lastMax)
+	return len(lastMaxString)
 }
 
 func main() {
 	lengthOfLongestSubstring(os.Args[1])
+
 }
