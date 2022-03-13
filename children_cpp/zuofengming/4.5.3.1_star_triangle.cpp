b9e8b9d101c4f567fb48512e80054e12bee83759
Bamvor ZHANG <bamv2005@gmail.com>
2022-03-13 19:56:02 +0800

    zuofengming: star triangle
    Signed-off-by: Bamvor ZHANG <bamv2005@gmail.com>

---
+++ children_cpp/zuofengming/4.5.3.1_star_triangle.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int i,j;
+	
+	cin>>n; 
+
+	for (i = 0; i < n; i++)
+	{
+		for ( j = 1; j < n - i; j++)
+		{
+			cout<<" ";
+		}
+		for ( j = 0; j < 2 * i + 1; j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
